999c8eba51a99cc05618032ef6eecc1fb9b31a1d
Deep Jadav <jadavdeep560@gmail.com>
2026-09-13 18:43:10 +0530

    Update frontend

--- config/mailer.js
+++ config/mailer.js
@@ -1,13 +1,11 @@
 const nodemailer = require('nodemailer');
 
-
-
 const transporter = nodemailer.createTransport({
-  host: '://gmail.com',
+  host: 'smtp.gmail.com',  // <-- FIXED: Changed from '://gmail.com'
   port: 587,
   secure: false,
   family: 4,
-  localAddress: '0.0.0.0', // <-- Add this line to force IPv4 routing
+  localAddress: '0.0.0.0', 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
